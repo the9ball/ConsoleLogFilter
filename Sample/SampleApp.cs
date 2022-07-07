d18c10fc7e0a7392f63c4bc101fb1c0ea2010744
Shoichi Yasui <the9ball@gmail.com>
2022-07-08 00:47:41 +0900

    Write random strings

--- Sample/SampleApp.cs
+++ Sample/SampleApp.cs
@@ -8,14 +8,27 @@ namespace Sample;
 /// </summary>
 internal class SampleApp : IHostedService
 {
+    private static readonly LogLevel[] LogLevels = (LogLevel[])Enum.GetValues(typeof(LogLevel));
+
     private readonly ILogger _logger;
 
     public SampleApp(ILogger<SampleApp> logger) => _logger = logger;
 
-    public Task StartAsync(CancellationToken cancellationToken)
+    public async Task StartAsync(CancellationToken cancellationToken)
     {
-        _logger.LogInformation(nameof(StartAsync));
-        return Task.CompletedTask;
+        var random = new Random();
+
+        try
+        {
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                var logLevel = LogLevels[random.Next(LogLevels.Length)];
+                Console.WriteLine("LogLevel -> " + logLevel);
+                _logger.Log(logLevel, "message {0}", logLevel);
+                await Task.Delay(TimeSpan.FromSeconds(1), cancellationToken);
+            }
+        }
+        catch (OperationCanceledException) { }
     }
 
     public Task StopAsync(CancellationToken cancellationToken)
